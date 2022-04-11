97d71347f8308b2762e7be7d4ddb8d1db748fceb
Emmanuel <EBuraimo@spartaglobal.com>
2022-04-11 01:07:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kotlinapplication/KotlinBasic.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinapplication
+
+
+
+    fun main()
+
+    {
+        var myName = "Ryan"
+        var greeting = "Hello World"
+        var firstChar = greeting.get(2)
+        println(firstChar)
+        print("Hello $myName")
+    }
+
+
